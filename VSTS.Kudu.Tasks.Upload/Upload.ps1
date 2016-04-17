c398c115a023d0337e022fc5a3f291a739468a37
Sergii Bomko <aquila.ua@gmail.com>
2016-04-17 11:51:12 +0300

    adds manifest for Upload task

--- VSTS.Kudu.Tasks.Upload/Upload.ps1
+++ VSTS.Kudu.Tasks.Upload/Upload.ps1
@@ -1,3 +1,30 @@
-#
-# Upload.ps1
-#
+[CmdletBinding(DefaultParameterSetName = 'None')]
+param
+(
+    [String] [Parameter(Mandatory = $true)]
+    $ConnectedServiceName,
+
+    [String] [Parameter(Mandatory = $true)]
+    $WebSiteName,
+
+    [String] [Parameter(Mandatory = $false)]
+    $Slot,
+
+    [String] [Parameter(Mandatory = $true)]
+    $Package,
+
+    [String] [Parameter(Mandatory = $true)]
+    $DestinationPath
+)
+
+Write-Verbose "Entering script Upload.ps1"
+
+Write-Host "ConnectedServiceName= $ConnectedServiceName"
+Write-Host "WebSiteName= $WebSiteName"
+Write-Host "Slot= $Slot"
+Write-Host "Package= $Package"
+Write-Host "DestinationPath= $DestinationPath"
+
+Write-Host "packageFile= Find-Files -SearchPattern $Package"
+$packageFile = Find-Files -SearchPattern $Package
+Write-Host "packageFile= $packageFile"
